210dfdc468211ff837d0f5b95cbb8b1d296a3fbd
Latapex <144073650+Latapex@users.noreply.github.com>
2025-08-06 15:55:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,10 +12,12 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('meu-pwa-cache-v1').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/main.css',
-        '/images/logo.png'
+        '/PWA_Service-Worker/',
+        '/PWA_Service-Worker/index.html',
+        '/PWA_Service-Worker/style.css',
+        '/PWA_Service-Worker/manifest.json',
+        '/PWA_Service-Worker/icons/icon-192x192.png',
+        '/PWA_Service-Worker/icons/icon-512x512.png'
       ]).catch((error) => {
         // Este console.error vai mostrar qual arquivo específico falhou
         console.error('Falha ao adicionar arquivos ao cache:', error);
@@ -32,4 +34,5 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
 
