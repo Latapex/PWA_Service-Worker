fb7fd1f247906c176a4e7488a84c18e0362b33d2
Latapex <144073650+Latapex@users.noreply.github.com>
2025-08-06 15:29:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-task-list-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/PWA_Service-Worker/',
+    '/PWA_Service-Worker/index.html',
+    '/PWA_Service-Worker/style.css',
+    '/PWA_Service-Worker/manifest.json',
+    '/PWA_Service-Worker/icons/icon-192x192.png',
+    '/PWA_Service-Worker/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -25,3 +25,4 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
